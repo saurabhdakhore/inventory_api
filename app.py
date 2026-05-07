5485dece6608ce6e7acdbf35ab68418dac032065
saurabhdakhore <46850287+saurabhdakhore@users.noreply.github.com>
2026-05-07 10:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-# ---------------- FIREBASE ----------------
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_json = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+
+cred = credentials.Certificate(firebase_json)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
